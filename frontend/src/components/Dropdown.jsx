61434b2b033e258bbee5480aa329a7a8f7f8be82
Alex Luo <alexluo92823@gmail.com>
2022-03-03 23:20:38 -0500

    deploy heroku

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useContext } from "react";
-import Checkbox from "./Checkbox";
+import React, { useState } from "react";
 import dropdownIcon from "../images/dropdownIcon.png";
 
 export default function Dropdown(props) {
