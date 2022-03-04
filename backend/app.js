992bec1825e077412164650cc3fc9a51f4267dc6
Alex Luo <alexluo92823@gmail.com>
2022-03-04 11:14:33 -0500

    follow guide on the internet

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const facetsRouter = require("./routes/facets");
 const attractionsRouter = require("./routes/attractions");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend/dist')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/dist/index.html'))
+})
 
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/facets", facetsRouter);
