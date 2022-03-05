23b585830580bba146e7073c3eeb32db85d60fbd
Alex Luo <alexluo92823@gmail.com>
2022-03-04 20:07:56 -0500

    add start script and express.static

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,9 @@ const attractionsRouter = require("./routes/attractions");
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/facets", facetsRouter);
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("/frontend/dist"))
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`)); // listen on proxy
