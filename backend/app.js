782690bf09e7b19ae537f51e3329c793115cb0eb
Alex Luo <alexluo92823@gmail.com>
2022-03-05 00:45:04 -0500

    asdf

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@ const path = require("path");
 const facetsRouter = require("./routes/facets");
 const attractionsRouter = require("./routes/attractions");
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/facets", facetsRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`)); // listen on proxy
