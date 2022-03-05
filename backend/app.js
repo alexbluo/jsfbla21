70c420406d954f24a486b92e126f01a4519d1b2b
Alex Luo <alexluo92823@gmail.com>
2022-03-04 23:29:22 -0500

    test deploying backend on heroku and static serving react build

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const facetsRouter = require("./routes/facets");
 const attractionsRouter = require("./routes/attractions");
 
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+});
+
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/facets", facetsRouter);
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("/frontend/dist"))
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`)); // listen on proxy
