be7e5d7156ec4b661118481c5c69d3338fa31a3c
Alex Luo <alexluo92823@gmail.com>
2022-03-05 00:30:29 -0500

    pathing stuff

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,13 @@ const facetsRouter = require("./routes/facets");
 const attractionsRouter = require("./routes/attractions");
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
-console.log(path.join(__dirname, "/frontend/build/index.html"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
-});
 
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/facets", facetsRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`)); // listen on proxy
