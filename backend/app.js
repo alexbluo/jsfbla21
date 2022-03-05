9ceda5f9ff3e22bf38db84ab03f224f0fc5bb373
Alex Luo <alexluo92823@gmail.com>
2022-03-05 00:05:53 -0500

    typo

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const facetsRouter = require("./routes/facets");
 const attractionsRouter = require("./routes/attractions");
 
-app.use(express.static("/frontend/dist")));
+app.use(express.static("/frontend/dist"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join("/frontend/dist/index.html"));
