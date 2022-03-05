721a5a7a5e34013553514f2c4e9ad972447fed11
Alex Luo <alexluo92823@gmail.com>
2022-03-05 00:05:14 -0500

    started up somehow, change pathing in app

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,10 @@ const path = require("path");
 const facetsRouter = require("./routes/facets");
 const attractionsRouter = require("./routes/attractions");
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use(express.static("/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/dist/index.html"));
+  res.sendFile(path.join("/frontend/dist/index.html"));
 });
 
 app.use("/api/attractions", attractionsRouter);
