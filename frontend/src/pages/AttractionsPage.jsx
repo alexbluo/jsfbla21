47c5733936e0e38a6259e3672f961454d7288d83
Alex Luo <alexluo92823@gmail.com>
2022-03-12 03:47:12 -0500

    start react query migration:

--- frontend/src/pages/AttractionsPage.jsx
+++ frontend/src/pages/AttractionsPage.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useQuery } from "react-query";
+import React, { useState, useEffect } from "react";
+import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 import Facets from "../components/Facets";
 import Preview from "../components/Preview";
 import NavBar from "../components/NavBar";
-
-// function fetchAttractions
 
 // query param string to be passed up from child checkboxes
 export const QueryParamContext = React.createContext({
@@ -14,73 +12,32 @@ export const QueryParamContext = React.createContext({
 });
 
 export default function AttractionsPage() {
-  const _isMounted = useRef(true);
-  const [previewData, setPreviewData] = useState(null);
-  const [pageNumber, setPageNumber] = useState(0);
-  const [hasNext, setHasNext] = useState(false);
-
   const [queryParam, setQueryParam] = useState("");
   const value = { queryParam, setQueryParam };
 
-  useEffect(() => {
-    return () => {
-      _isMounted.current = false;
-    };
-  }, []);
-
-  useEffect(async () => {
-    const res = await axios.get(
-      `/api/attractions?page=${pageNumber}${queryParam}`
-    );
-
-    // check if page is still mounted and state can be updated
-    if (_isMounted.current) {
-      if (pageNumber === 0) {
-        // if page number is zero then don't use previous data
-        setPreviewData(res.data.previewData);
-        setHasNext(res.data.hasNext);
-      } else {
-        // otherwise paginate by updating preview data with both new and previous data
-        setPreviewData((prev) => [...prev, ...res.data.previewData]);
-        setHasNext(res.data.hasNext);
-      }
+  const {
+    data,
+    error,
+    isLoading,
+    isError,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ["attractions", queryParam],
+    async (pageNumber = 0) => {
+      const res = await axios.get(
+        `/api/attractions?page=${pageNumber}${queryParam}`
+      );
+      return res.data;
+    },
+    {
+      getNextPageParam: (prev) => prev + 1,
     }
-  }, [pageNumber, queryParam]);
-
-  useEffect(() => {
-    if (_isMounted.current) {
-      setPageNumber(0);
-    }
-  }, [queryParam]);
-
-  function renderPreviews() {
-    return previewData && previewData.length === 0 ? (
-      <p>Nothing Matched!</p>
-    ) : (
-      <div className="grid grid-cols-2 gap-16">
-        {previewData &&
-          previewData.map((doc) => (
-            <Preview data={doc} key={doc.attraction_id} />
-          ))}
-      </div>
-    );
-  }
-
-  /**
-   * @returns a load more button if all conditions are met, otherwise nothing is returned
-   */
-  function renderLoadMoreButton() {
-    return (
-      hasNext && (
-        <button
-          className="px-4 py-2 mt-8 text-white rounded-md bg-red"
-          onClick={() => setPageNumber((prev) => prev + 1)}
-        >
-          Load More
-        </button>
-      )
-    );
-  }
+  );
+  if (isLoading) return null;
+  if (isError) return <span>Error: {error.message}</span>;
 
   return (
     <div className="content-body-container">
@@ -91,8 +48,21 @@ export default function AttractionsPage() {
           <Facets />
         </QueryParamContext.Provider>
         <div className="relative flex flex-col items-center w-[69%]">
-          {renderPreviews()}
-          {renderLoadMoreButton()}
+          <div className="grid grid-cols-2 gap-16">
+            {data.pages.map((group) =>
+              group.previewData.map((doc) => (
+                <Preview data={doc} key={doc.attraction_id} />
+              ))
+            )}
+          </div>
+          {hasNextPage && (
+            <button
+              className="px-4 py-2 mt-8 text-white rounded-md bg-red"
+              onClick={() => fetchNextPage()}
+            >
+              Load More
+            </button>
+          )}
         </div>
       </div>
     </div>
