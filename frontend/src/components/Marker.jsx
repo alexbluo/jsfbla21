4fd94eacb79079b3f72e34f30b5d7fd5df51fa70
Alex Luo <alexluo92823@gmail.com>
2022-03-15 00:24:15 -0400

    center window above markers

--- frontend/src/components/Marker.jsx
+++ frontend/src/components/Marker.jsx
@@ -4,9 +4,9 @@ export default function Marker(props) {
   const [show, setShow] = useState(false);
 
   return (
-    <div className="">
+    <div className="relative flex flex-col-reverse items-center">
       <div
-        className={`w-4 z-10 aspect-square border-2 rounded-full cursor-pointer ${
+        className={`absolute w-4 z-10 aspect-square border-2 rounded-full cursor-pointer ${
           props.isCenter ? "bg-white border-red" : "bg-gold border-black"
         } hover:brightness-50 duration-300 ease-out`}
         onClick={props.onClick}
@@ -14,7 +14,7 @@ export default function Marker(props) {
         onMouseLeave={() => setShow(false)}
       ></div>
       {show && (
-        <div className=" w-32 h-16 origin-center bg-red animate-spring-scale-up"></div>
+        <div className="absolute w-32 h-16 origin-bottom rounded-lg -top-20 bg-red animate-spring-scale-up"></div>
       )}
     </div>
   );
