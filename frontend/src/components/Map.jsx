5f6eff83062213ddd02ddd99870673e2e891d7b2
Alex Luo <alexluo92823@gmail.com>
2022-03-16 13:29:38 -0400

    center slider with input

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -25,12 +25,12 @@ export default function Map({ center }) {
     if (value > 500) value = 500;
     if (value < 0) value = 0;
     setSliderValue(value);
-    setSearchRadius(value * 1000);
+    // setSearchRadius(value * 1000);
   }
 
   if (isError) return <span>Error: {error.message}</span>;
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row justify-between">
       <div className="w-1/2 aspect-square">
         <GoogleMapReact
           bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY }}
@@ -51,44 +51,44 @@ export default function Map({ center }) {
         </GoogleMapReact>
       </div>
 
-      <div className="w-1/2 pl-8">
-        <label>
-          <input
-            type="number"
+      <div className="relative w-5/12">
+        <div className="absolute top-0 left-0 flex flex-row items-center w-full">
+          <label className="flex mr-2">
+            <input
+              className="w-10 mr-1 text-center border-2 rounded"
+              type="number"
+              min={0}
+              max={500}
+              value={sliderValue}
+              onInput={handleInput}
+            />
+            km
+          </label>
+          <Slider
+            className=""
             min={0}
             max={500}
             value={sliderValue}
-            onInput={handleInput}
+            onChange={(value) => setSliderValue(value)}
+            onAfterChange={() => setSearchRadius(sliderValue * 1000)}
+            railStyle={{
+              backgroundColor: "black",
+              height: 3,
+              marginTop: 1,
+            }}
+            trackStyle={{
+              backgroundColor: "gold",
+              marginLeft: -1,
+            }}
+            handleStyle={{
+              height: 15,
+              width: 15,
+              borderRadius: "50%",
+              borderColor: "gold",
+              backgroundColor: "gold",
+            }}
           />
-          km
-        </label>
-        <Slider
-          className=""
-          min={0}
-          max={500}
-          value={sliderValue}
-          onChange={(value) => setSliderValue(value)}
-          onAfterChange={() => setSearchRadius(sliderValue * 1000)}
-          railStyle={{
-            backgroundColor: "var(--flag-gold)",
-            height: 2,
-          }}
-          trackStyle={{
-            backgroundColor: "var(--flag-black)",
-            marginTop: -1,
-            marginLeft: 1,
-            width: 90,
-            height: 4,
-          }}
-          handleStyle={{
-            height: 14,
-            width: 14,
-            marginTop: -6,
-            borderRadius: "50%",
-            borderColor: "var(--flag-black)",
-            backgroundColor: "var(--flag-gold)",
-          }}
-        />
+        </div>
       </div>
     </div>
   );
