12b90e4d1fb07af6d84f5899002e8cab06dd415f
Alex Luo <alexluo92823@gmail.com>
2022-03-25 12:12:54 -0400

    edit landing page title, change dropdown icon

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 import Checkbox from "./Checkbox";
-import dropdownIcon from "../images/dropdownIcon.png";
+import arrow from "../images/arrow.png";
 
 export default function Dropdown(props) {
   const [isOpened, setIsOpened] = useState(false);
@@ -20,25 +20,27 @@ export default function Dropdown(props) {
   return (
     <div className="bg-black">
       <div
-        className="flex items-center p-3 rounded cursor-pointer select-none bg-gold"
+        className="flex items-center justify-between p-3 rounded cursor-pointer select-none bg-gold"
         onClick={() => setIsOpened(!isOpened)}
       >
         <h2 className="text-lg text-black font-raleway">
           {props.category.toUpperCase()}
         </h2>
         <img
-          className={`w-3 h-3 ml-2 transition-transform ${
-            isOpened && "rotate-90"
+          className={`w-6 h-6 duration-300 ease-out transition-transform ${
+            isOpened && "rotate-180"
           }`}
-          src={dropdownIcon}
+          src={arrow}
           alt=""
         />
       </div>
+      {/* TODO: fetch in facets and map so that this can be reused in help page */}
       <ul
         className={`${
           isOpened ? "max-h-48 my-4" : "max-h-0 overflow-hidden"
         } font-raleway text-base text-gold mx-4 overflow-y-auto duration-500 ease-[cubic-bezier(0.34, 1.56, 0.64, 1)]`}
       >
+        
         {/* map fields that fall under the category to checkboxes */}
         {data[props.category].map((field, index) => (
           <Checkbox category={props.category} field={field} key={index} />
