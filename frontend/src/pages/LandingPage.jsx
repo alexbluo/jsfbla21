6880b4aa6b6308d4353cc058b6075b9bb265d13c
Alex Luo <alexluo92823@gmail.com>
2022-05-02 17:37:59 -0400

    test if title displays in production

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
   return (
     <div className="h-screen w-screen bg-gradient-to-br from-gold to-red">
       <div className="fixed z-10 flex h-full w-full flex-col items-center justify-center">
-        <h1 className="mb-6 animate-fade-in-down select-none bg-[url('src/images/MDFlagGIF.gif')] bg-clip-text bg-top py-1 font-montserrat text-5xl font-[1000] text-transparent opacity-90">
+        <h1 className="mb-6 animate-fade-in-down select-none bg-[url('images/MDFlagGIF.gif')] bg-clip-text bg-top py-1 font-montserrat text-5xl font-[1000] text-transparent opacity-90">
           Maryland Attractions Searcher
         </h1>
         <Link to="/attractions">
