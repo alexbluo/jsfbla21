ae5f4ef3219f5a8ddd6f548f30a471c0c5205495
Alex Luo <alexbluo01@gmail.com>
2022-06-05 18:07:11 -0400

    fix details section scroll

--- frontend/src/components/DetailSection.jsx
+++ frontend/src/components/DetailSection.jsx
@@ -4,13 +4,13 @@ const DetailSection = ({ header, order, children }) => {
   return (
     <div
       className={classNames(
-        "flex aspect-square flex-col gap-2 overflow-y-scroll py-4 px-8",
+        "flex aspect-square flex-col gap-2 overflow-auto py-4 px-8",
         { "bg-gold text-black": order === "odd" },
         { "bg-red text-white": order === "even" }
       )}
     >
       <h2 className="text-2xl">{header}</h2>
-      {children}
+      <div className="box-border h-full overflow-auto">{children}</div>
     </div>
   );
 };
