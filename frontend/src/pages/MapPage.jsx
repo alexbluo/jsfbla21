e50b11368df3edd69f840f4a9b708064af7849af
Alex Luo <alexbluo01@gmail.com>
2022-06-07 08:13:44 -0400

    add checking if the user is nearby

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import qs from "qs";
 import Map from "../components/Map";
 
 const MapPage = () => {
@@ -9,12 +11,21 @@ const MapPage = () => {
   // TODO: figure out not in maryland situation
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
+      navigator.geolocation.getCurrentPosition(async (position) => {
         const coords = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        setCenter(coords);
+
+        const params = qs.stringify({
+          search: "",
+          ...coords,
+          searchRadius: 20000,
+        });
+        // check if the user is near maryland by checking if there is a nearby attraction
+        const res = await axios.get(`/api/attractions/near?${params}`);
+        // set the center to the user's location only if they are nearby, otherwise stay with default
+        if (res.data.length > 0) setCenter(coords);
       });
     }
   }, []);
