dbd628e3eeb37ff70fefd93e02b6ea41b6fad20e
Alex Luo <alexbluo01@gmail.com>
2022-06-16 00:23:43 -0400

    add vite code splitting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { dependencies } from "./package.json";
+
+const renderChunks = (deps) => {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+};
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +18,17 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5000/",
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
       },
     },
   },
