97d0586d80e1cc005cd3b21848a3408a320a32c3
Alex Luo <alexbluo01@gmail.com>
2022-06-16 12:52:15 -0400

    add documentation for code splitting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,21 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { dependencies } from "./package.json";
 
+/**
+ * Formats the dependencies for code-splitting
+ * @param { Object<string, string> } deps the list of dependencies from package.json
+ * @returns { Object<string, Array<string>> } the list of code chunks to be split
+ */
 const renderChunks = (deps) => {
   const chunks = {};
+
   Object.keys(deps).forEach((key) => {
+    // ignore react dependencies, which are already included in vendor
     if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    // otherwise add the dependency to the chunks object
     chunks[key] = [key];
   });
+
   return chunks;
 };
 
