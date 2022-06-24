320b13edca86b19754e6b70e169ae3a7a7502760
Alex Luo <alexbluo01@gmail.com>
2022-06-23 21:03:14 -0400

    fix safari preview preloader

--- frontend/src/components/PreviewPreloader.jsx
+++ frontend/src/components/PreviewPreloader.jsx
@@ -2,10 +2,13 @@ import ContentLoader from "react-content-loader";
 
 export default function PreviewPreloader(props) {
   return (
-    <ContentLoader viewBox="0 0 300 300" {...props}>
-      <rect x="0" y="0" rx="10" ry="10" width="300" height="240" />
-      <rect x="0" y="250" rx="5" ry="5" width="300" height="20" />
-      <rect x="0" y="280" rx="5" ry="5" width="300" height="20" />
-    </ContentLoader>
+    // div required for safari
+    <div>
+      <ContentLoader viewBox="0 0 300 300" {...props}>
+        <rect x="0" y="0" rx="10" ry="10" width="300" height="240" />
+        <rect x="0" y="250" rx="5" ry="5" width="300" height="20" />
+        <rect x="0" y="280" rx="5" ry="5" width="300" height="20" />
+      </ContentLoader>
+    </div>
   );
 }
