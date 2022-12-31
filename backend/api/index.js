50c78d71db328ed3fbdacdce7e458866efb85afd
Alex Luo <alexbluo01@gmail.com>
2022-12-30 20:44:18 -0500

    export app

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,5 +10,4 @@ app.use(logger("dev"));
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/filters", filtersRouter);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
+module.exports = app;
