beb1b64aeedc96980db39a5bc358da84de21e26c
Alex Luo <alexbluo01@gmail.com>
2023-04-19 15:13:56 -0400

    fix weird 404 bug:

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const attractionsRouter = require("../routes/attractions");
@@ -11,11 +10,6 @@ app.use(logger("dev"));
 app.use("/api/attractions", attractionsRouter);
 app.use("/api/filters", filtersRouter);
 
-// catch all other requests
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+app.listen(5000)
 
 module.exports = app;
-
-// app.listen(5000)
